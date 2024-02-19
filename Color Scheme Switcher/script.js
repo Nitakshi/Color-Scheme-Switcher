0c69cf3f5466a655d9064845dffdb85997c69528
Nitakshi Azad <119751355+Nitakshi@users.noreply.github.com>
2024-02-19 17:49:28 +0530

    script.js

--- Color Scheme Switcher/script.js
+++ Color Scheme Switcher/script.js
@@ -1,6 +1,7 @@
 let buttons=document.querySelectorAll('.button');
 let body=document.querySelector('body');
 
+
 buttons.forEach(function (button){
       button.addEventListener('click',function(event){
          if(event.target.id === "grey"){
@@ -26,6 +27,35 @@ buttons.forEach(function (button){
          else{
             body.style.backgroundColor="blanchedalmond";
             body.style.color="black";
+            
          }
       })
+      
+})
+ 
+//random Color generate
+const randomcolor=function(){
+   const hex='0123456789ABCDEF';
+   let  color='#';
+   for(let i=0;i<6;i++){
+      color+= hex[Math.floor(Math.random()*16)]; //get values till 16
+   }
+   return color; //mnn e mnn m generate krke kya fayda :) return krdo
+}
+let intervalid;
+
+let startchangingcolor= function(){
+   if(!intervalid){
+      document.body.style.backgroundColor=randomcolor();
+   }
+   intervalid=setInterval(()=>{
+      document.body.style.backgroundColor=randomcolor();
+   },1000);
+   
+}
+
+let start=document.querySelector("#start").addEventListener("click",startchangingcolor)
+document.querySelector('#stop').addEventListener("click",()=>{
+        clearInterval(intervalid);
+        intervalid=null; //dereference
 })
